9b8e4e0a750e693da720e002458801d2b124eb2b
Zaida Gonzalez <yasmin.gonzalez@globallogic.com>
2026-06-25 12:21:29 -0300

    Add new orders history test

--- pages/OrdersHistoryPage.ts
+++ pages/OrdersHistoryPage.ts
@@ -4,12 +4,19 @@ import { BasePage } from "./BasePage";
 class OrdersHistoryPage extends BasePage{
 
     displayedOrders:any    
+    backToShopBtn:any
 
     constructor(page:any){
         super(page)
-        this.displayedOrders = page.locator(".ng-star-inserted")
+        this.displayedOrders = page.locator("tbody")
+        this.backToShopBtn = page.getByRole('button', {name: 'Go Back to Shop'})
+//getByRole('rowheader', { name: '6a3d3c8c378febeacdcdbbad' })
 
+    }
 
+    async getLastOrderHistoryText(item:string){
+        const text = await this.displayedOrders.allTextContents()
+        return (text.toString()).split(item)[0]
     }
 }
 
